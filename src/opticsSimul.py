8286028663329cac4091fac423177cad9e8c88bc
Gonzalez-MSI <115946547+Gonzalez-MSI@users.noreply.github.com>
2024-11-13 20:17:28 -0600

    Optics Simulation With LightPipes

--- src/opticsSimul.py
+++ src/opticsSimul.py
@@ -1,3 +1,50 @@
+"""
+Optical Interference Pattern Simulation
+This script simulates interference between two Gaussian laser beams using LightPipes library.
+"""
+
+# Function Arguments Documentation:
+
+# LightPipes.Begin(GridSize, wavelength, GridDimension)
+# - GridSize: Physical size of simulation window (30mm)
+# - wavelength: Laser wavelength (633nm - red light)
+# - GridDimension: Number of pixels (250x250 grid)
+
+# LightPipes.GaussBeam(Field, w0)
+# - Field: Input field from Begin()
+# - w0: Beam waist radius (8mm) - radius where intensity falls to 1/e²
+
+# LightPipes.Tilt(Field, angle, direction)
+# - Field: Input field
+# - angle: Tilt angle (0.5 milliradians)
+# - direction: 0 for x-axis tilt, π/2 for y-axis tilt
+
+# LightPipes.BeamMix(Field1, Field2)
+# - Field1, Field2: Two fields to be coherently combined
+# Returns: Complex sum of fields
+
+# LightPipes.Intensity(Field)
+# - Field: Input field
+# Returns: 2D array of intensity values
+
+# plt.imshow(data, cmap, extent)
+# - data: 2D intensity array
+# - cmap: Color map ('jet' = blue-to-red)
+# - extent: Physical dimensions [-15mm to +15mm]
+
+"""
+Units used:
+- LightPipes.mm: millimeters
+- LightPipes.nm: nanometers
+- LightPipes.mrad: milliradians
+
+The simulation creates:
+1. Two identical Gaussian beams (8mm waist)
+2. Adds angular tilt to second beam (0.5mrad)
+3. Combines beams to create interference
+4. Plots resulting intensity pattern
+"""
+
 import LightPipes
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,11 +56,11 @@ wavelength = 633*(LightPipes.nm)
 
 # Create first field
 Field1 = LightPipes.Begin(GridSize, wavelength, GridDimension)
-Field1 = LightPipes.GaussBeam(Field1, w0=5*LightPipes.mm)
+Field1 = LightPipes.GaussBeam(Field1, w0=8*LightPipes.mm)
 
 # Create second field with offset
 Field2 = LightPipes.Begin(GridSize, wavelength, GridDimension)
-Field2 = LightPipes.GaussBeam(Field2, w0=5*LightPipes.mm)
+Field2 = LightPipes.GaussBeam(Field2, w0=8*LightPipes.mm)
 Field2 = LightPipes.Tilt(Field2, 0.5*LightPipes.mrad, 0) # Add tilt for interference
 
 # Combine fields
